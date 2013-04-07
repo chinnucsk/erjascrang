c5a22fad655947534848ee080af762ba9bc3ff44
Tyler Boyd <tboyd47@Tylers-MacBook-Air.local>
2013-04-07 13:38:02 -0400

    add early version of erjascrang -- on-the-fly static typing in JS

---
+++ erjascrang.js
@@ -0,0 +1,67 @@
+var Erjascrang = function(init) {
+  init.call(this);
+
+  // default to using JS's built-in types
+  this.types = {
+    "b": function(x) { return typeof x == 'boolean' },
+    "o": function(x) { return typeof x == 'object' },
+    "n": function(x) { return typeof x == 'number' },
+    "u": function(x) { return typeof x == 'undefined' },
+    "s": function(x) { return typeof x == 'string' },
+    "$": function(x) { return true },
+  };
+
+  // return an object with all compiled methods with added type
+  // checking.
+  this.compile = function() {
+    var result = {};
+    var normalizedMethodNames = [];
+
+    for(var methodName in this) {
+      for(var t in this.types) {
+        var matcher = new RegExp("_" + t + "$");
+
+        if(methodName.match(matcher)) {
+          var normalized = methodName.replace(matcher, "");
+          if(normalizedMethodNames.indexOf(normalized) == -1) {
+            normalizedMethodNames.push(normalized);
+          }
+        }
+      }
+    }
+
+    for(var n=0; n<normalizedMethodNames.length; n++) {
+      var name = normalizedMethodNames[n];
+      var source = this;
+
+      result[name] = function(x) {
+        for(var i in source.types) {
+          if(source.types[i](x)) {
+            if(source[name + "_" + i]) {
+              return source[name + "_" + i].call(this, x);
+            }
+          }
+        }
+        if(source[name + "_$"]) {
+          return source[name + "_$"].call(this, x);
+        } else {
+          return undefined;
+        }
+      }
+    };
+
+    return result;
+  };
+};
+
+var erjascrang = new Erjascrang(function() {
+  this.addTwo_s = function(s) { return this.addTwo(parseInt(s)); }
+  this.addTwo_n = function(n) { return n+2; }
+  this.addTwo_$ = function(n) { return 0; }
+});
+
+var compiled = erjascrang.compile();
+
+for(var m in compiled) {
+  this[m] = compiled[m];
+}
